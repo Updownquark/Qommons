3328d755720875dc4157b01d7debe05f51f8c9fc
Updownquark <Updownquark@gmail.com>
2017-03-17 11:47:05 -0600

    Some improvements to the hierarchical data source interfaces

--- src/main/java/org/qommons/io/DefaultFileSource.java
+++ src/main/java/org/qommons/io/DefaultFileSource.java
@@ -8,6 +8,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 /** A simple resource reader/writer that uses a directory */
 public class DefaultFileSource implements HierarchicalResourceReader, HierarchicalResourceWriter {
@@ -60,6 +64,22 @@ public class DefaultFileSource implements HierarchicalResourceReader, Hierarchic
     }
 
     @Override
+	public Collection<String> getSubDirs(String path) throws IOException {
+		File subDir = new File(theDir, path);
+		if (!subDir.exists() || !subDir.isDirectory())
+			return Collections.emptyList();
+		return Arrays.stream(subDir.listFiles()).filter(f -> f.isDirectory()).map(f -> f.getName()).collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<String> getResources(String subDir) throws IOException {
+		File subDirectory = new File(theDir, subDir);
+		if (!subDirectory.exists() || !subDirectory.isDirectory())
+			return Collections.emptyList();
+		return Arrays.stream(subDirectory.listFiles()).filter(f -> !f.isDirectory()).map(f -> f.getName()).collect(Collectors.toList());
+	}
+
+	@Override
     public String toString() {
         return theDir.getPath();
     }
