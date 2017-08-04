9b4ded53c75470dcee92c5956d59e2f95cf44e94
Andrew <Updownquark@gmail.com>
2017-08-03 22:26:49 -0600

    Added test for BetterHashMap

--- src/test/java/org/qommons/collect/HashSetTest.java
+++ src/test/java/org/qommons/collect/HashSetTest.java
@@ -3,9 +3,17 @@ package org.qommons.collect;
 import org.junit.Test;
 import org.qommons.QommonsTestUtils;
 
+/** Tests {@link BetterHashSet} and {@link BetterHashMap} */
 public class HashSetTest {
+	/** Tests {@link BetterHashSet} */
 	@Test
 	public void testHashSet() {
 		QommonsTestUtils.testCollection(BetterHashSet.build().unsafe().buildSet(), null, null);
+	}
+
+	/** Tests {@link BetterHashMap} */
+	@Test
+	public void testHashMap() {
+		QommonsTestUtils.testMap(BetterHashMap.build().unsafe().buildMap(), null, null);
 	}
 }
