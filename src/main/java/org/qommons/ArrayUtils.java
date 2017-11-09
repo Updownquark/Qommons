377b0ca26b78d25377ad3eaadfbcf39294c4a17c
abutler <abutler@psl.nmsu.edu>
2017-11-09 09:42:58 -0700

    Changed the javadoc of the ArrayUtils.adjust method to point to the new Qommons wiki instead of the old, immutable Google Code link.

--- src/main/java/org/qommons/ArrayUtils.java
+++ src/main/java/org/qommons/ArrayUtils.java
@@ -1450,7 +1450,7 @@ public final class ArrayUtils {
 	 * </p>
 	 * <p>
 	 * For more information, see
-	 * <a href="http://code.google.com/p/prisms/wiki/ArrayUtils">http://code.google.com/p/prisms/wiki/ArrayUtils</a>
+	 * <a href="https://github.com/Updownquark/Qommons/wiki/ArrayUtils">https://github.com/Updownquark/Qommons/wiki/ArrayUtils</a>
 	 * </p>
 	 *
 	 * @param <T1> The type of the original array
