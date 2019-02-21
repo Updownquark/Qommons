f3c3df0e23adb3738771f2af367e970f8e47af89
abutler <abutler@psl.nmsu.edu>
2019-02-21 16:30:35 -0700

    Fixed some bugs in CsvParser (by rewriting a lot of the state to a way simpler form) and wrote a unit test for it

--- src/main/java/org/qommons/io/CsvParser.java
+++ src/main/java/org/qommons/io/CsvParser.java
@@ -6,14 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A simple CSV parser. Attempts to be as tolerant as possible within the RFC 4180, tolerating quoted values containing any characters
- * (except unescaped double-quotes), doubled double-quotes (escaped double quote character), and backslash-escaped quotes.
+ * A simple CSV parser. Attempts to be as tolerant and flexible as possible, while adhering closely to the RFC 4180 standard. Some
+ * differences from the standard include:
+ * <ul>
+ * <li>The delimiter character is configurable</li>
+ * <li>Carriage returns (\r, ASCII code 13) are not required at the end of lines. Newline characters (\n, ASCII code 10) are required.</li>
+ * <li>Blank lines are allowed and ignored anywhere in the file (the {@link #getPassedBlankLines()} method may be used to detect when this
+ * happens)</li>
+ * <li>Un-quoted columns may include any character that is not the delimiter or the newline character</li>
+ * <li>Quoted columns may include any character at all (double-quote characters (") must, of course, be escaped by using a double
+ * double-quote (""))</li>
+ * <li>Depending on how it is called, the parser may or may not expect the same number of columns per line. The {@link #parseNextLine()}
+ * method parses a line regardless of the number of columns and returns them. The {@link #parseNextLine(String[])} method takes an array of
+ * columns and populates them with the columns found on the current line. If the number of columns found differs from the length of the
+ * input array, an exception is thrown.</li>
+ * </ul>
+ * <p>
+ * As per the standard, double double-quotes ("") within quoted columns are interpreted as a single double-quote character ("). But within
+ * un-quoted columns, double-quote characters are not special, so double double-quotes would be interpreted as-is ("").
+ * </p>
+ * <p>
+ * This class does not deal specifically with a header, which may be parsed the same as any other line.
+ * </p>
  */
 public class CsvParser {
 	final Reader theReader;
 	final char theDelimiter;
 	int theTabColumnOffset;
 	private final CsvParseState theParseState;
+	private int thePassedBlankLines;
 
 	/**
 	 * @param reader The reader to parse CSV data from
@@ -42,17 +63,20 @@ public class CsvParser {
 	}
 
 	/**
-	 * Parses all columns in the current line
+	 * Parses all columns in the next line
 	 * 
-	 * @return The parsed column values
+	 * @return The parsed column values for the line, or null if there is no more content in the file
 	 * @throws IOException If the reader throws an exception
-	 * @throws TextParseException If there is an error in the CSV file
+	 * @throws TextParseException If there is an error on the next line the CSV file
 	 */
 	public String[] parseNextLine() throws IOException, TextParseException {
+		thePassedBlankLines = 0;
 		List<String> columns = new ArrayList<>();
 		String value = theParseState.parseColumn();
-		while (value.length() == 0 && theParseState.getLastTerminal() == CsvValueTerminal.LINE_END)
-			value = theParseState.parseColumn(); // Move past any initial blank lines
+		while (value.length() == 0 && theParseState.getLastTerminal() == CsvValueTerminal.LINE_END) {
+			thePassedBlankLines++;
+			value = theParseState.parseColumn(); // Move past any blank lines
+		}
 		if (value.length() == 0 && theParseState.getLastTerminal() == CsvValueTerminal.FILE_END)
 			return null;
 
@@ -64,18 +88,22 @@ public class CsvParser {
 	}
 
 	/**
-	 * Parses the current line in the file, assuming the number of columns is equal to the length of the <code>columns</code> array
+	 * Parses the next line in the file, asserting that the number of columns is equal to the length of the <code>columns</code> array
 	 * 
-	 * @param columns The columns array to populate with the parsed column values
-	 * @return The parsed column values
+	 * @param columns The columns array to populate with the parsed column values from the next line of the file
+	 * @return True if content was parsed, or false if there is no more content in the file
 	 * @throws IOException If the reader throws an exception
-	 * @throws TextParseException If there is an error in the CSV file
+	 * @throws TextParseException If there is an error on the next line of the CSV file or the number of columns on the line is different
+	 *         than <code>columns.length</code>
 	 */
 	public boolean parseNextLine(String[] columns) throws IOException, TextParseException {
+		thePassedBlankLines = 0;
 		String value = theParseState.parseColumn();
-		while (theParseState.getLastTerminal() == CsvValueTerminal.LINE_END)
-			value = theParseState.parseColumn(); // Move past any initial blank lines
-		if (theParseState.getLastTerminal() == CsvValueTerminal.FILE_END)
+		while (value.length() == 0 && theParseState.getLastTerminal() == CsvValueTerminal.LINE_END) {
+			thePassedBlankLines++;
+			value = theParseState.parseColumn(); // Move past any blank lines
+		}
+		if (value.length() == 0 && theParseState.getLastTerminal() == CsvValueTerminal.FILE_END)
 			return false;
 
 		columns[0] = value;
@@ -90,15 +118,18 @@ public class CsvParser {
 		return true;
 	}
 
+	/** @return The number of blank lines that were ignored prior to the most recently-parsed line of the file */
+	public int getPassedBlankLines() {
+		return thePassedBlankLines;
+	}
+
 	static enum CsvValueTerminal {
 		COLUMN_END, LINE_END, FILE_END;
 	}
 
 	class CsvParseState {
 		private final StringBuilder theValue;
-
 		private int[] isQuoted;
-		private char theHoldover;
 		private int theOffset;
 		private int theLineNumber;
 		private int theColumnNumber; // Char columns, not CSV columns here
@@ -108,132 +139,91 @@ public class CsvParser {
 			theValue = new StringBuilder();
 		}
 
+		<T> T throwParseException(String message) throws TextParseException {
+			throw new TextParseException(message, theOffset, theLineNumber, theColumnNumber);
+		}
+
 		CsvValueTerminal getLastTerminal() {
 			return theLastTerminal;
 		}
 
 		String parseColumn() throws IOException, TextParseException {
-			if (beginColumn()) {
-				boolean doNothing = true;
-				while (doNothing && readNextChar()) {
-					doNothing = true; // Just here so one can step through each character as it's parsed
-				}
+			isQuoted = null;
+			theLastTerminal = null;
+			int c = readContentChar(true);
+			while (c >= 0) {
+				theValue.append((char) c);
+				c = readContentChar(false);
 			}
 			String column = theValue.toString();
 			theValue.setLength(0);
 			return column;
 		}
 
-		<T> T throwParseException(String message) throws TextParseException {
-			throw new TextParseException(message, theOffset, theLineNumber, theColumnNumber);
+		private int readContentChar(boolean columnStart) throws IOException, TextParseException {
+			int c = readStreamChar();
+			return interpretStreamChar(c, columnStart);
 		}
 
-		private boolean beginColumn() throws IOException, TextParseException {
-			theLastTerminal = null;
-			isQuoted = null;
-			int c = read();
-			if (isLineTerminal(c)) {
-				return false;
-			} else if (c == '"') {
-				int[] quotePosition = new int[] { theOffset, theLineNumber, theColumnNumber };
-				int moreQuotes = countConsecutiveQuotes();
-				for (int i = 0; i < moreQuotes; i += 2)
-					theValue.append('"');
-				if (moreQuotes % 2 == 0)
-					isQuoted = quotePosition;
-				return theLastTerminal == null;
-			} else if (c == theDelimiter) {
-				theLastTerminal = CsvValueTerminal.COLUMN_END;
-				return false;
-			} else
-				theValue.append((char) c);
-			return true;
-		}
-
-		private boolean readNextChar() throws IOException, TextParseException {
-			int c = read();
-			if (c == '"') {
-				int moreQuotes = countConsecutiveQuotes();
-				boolean endQuote = moreQuotes % 2 == 0;
-				if (endQuote) {
-					if (isQuoted != null) {
-						// End of the quoted value. Make sure the next character is the delimiter (or a value terminal).
-						c = read();
-						if (c == theDelimiter) {
-							theLastTerminal = CsvValueTerminal.COLUMN_END;
-							return false;
-						} else if (isLineTerminal(c))
-							return false;
-						else
-							return throwParseException("Unexpected content after end quote");
-					} else
-						return throwParseException("Bad placement of quote character");
-				}
-				for (int i = 0; i < moreQuotes; i += 2)
-					theValue.append('"');
-			} else if (isQuoted != null) {
-				if (c < 0)
-					throw new TextParseException("Unmatched quote character", isQuoted[0], isQuoted[1], isQuoted[2]);
-				theValue.append((char) c);
-				return true;
-			} else if (isLineTerminal(c)) {
-				return false;
-			} else if (c == theDelimiter) {
-				theLastTerminal = CsvValueTerminal.COLUMN_END;
-				return false;
-			} else
-				theValue.append((char) c);
-			return true;
-		}
-
-		private int read() throws IOException {
-			if (theHoldover > 0) {
-				char holdover = theHoldover;
-				theHoldover = 0;
-				return holdover;
-			}
+		private int interpretStreamChar(int c, boolean columnStart) throws IOException, TextParseException {
 			theOffset++;
-			int c = theReader.read();
-			if (c < 0)
-				theLastTerminal = CsvValueTerminal.FILE_END;
-			else if (c == '\n' || c == '\r') {
+			if (c == '\n') {
 				theLineNumber++;
 				theColumnNumber = 0;
-				// Parse out DOS-style line endings
-				int c1 = theReader.read();
-				if (c1 < 0)
-					theLastTerminal = CsvValueTerminal.FILE_END;
-				else if (isQuoted == null && c1 != c && (c1 == '\n' || c1 == '\r')) {
-					// Only swallow the extra newline character if we're not quoted
-				} else {
-					// c1 is a character in the next line
-					theHoldover = (char) c1;
-				}
-				if (isQuoted == null)
-					theLastTerminal = CsvValueTerminal.LINE_END;
-			} else if (c == '\t' && theTabColumnOffset != 1) {
-				int mod = theColumnNumber % theTabColumnOffset;
-				theColumnNumber += (theTabColumnOffset - mod);
-			} else {
+			} else
 				theColumnNumber++;
+
+			if (c == '"') {
+				if (columnStart) { // Begin quote
+					isQuoted = new int[] { theOffset, theLineNumber, theColumnNumber };
+					return readContentChar(false);
+				} else if (isQuoted != null) {
+					c = readStreamChar();
+					if (c == '"') { // Double double-quotes within a quoted column is an escaped double-quote
+						theColumnNumber++;
+						return c;
+					} else { // End quote
+						isQuoted = null;
+						c = interpretStreamChar(c, false);
+						if (c >= 0)
+							return throwParseException(
+								"Unexpected content after end quote: '" + (char) c + "'. End quotes must terminate the column.");
+						return c;
+					}
+				} else
+					return c;
+			} else if (c < 0) { // File end
+				if (isQuoted != null)
+					throw new TextParseException("Unmatched quote", isQuoted[0], isQuoted[1], isQuoted[2]);
+				theLastTerminal = CsvValueTerminal.FILE_END;
+				return -1;
+			} else if (c == '\t' && theTabColumnOffset != 1) { // Tab
+				int mod = (theColumnNumber - 1) % theTabColumnOffset;
+				theColumnNumber += (theTabColumnOffset - mod);
+			} else if (isQuoted != null) {
+				// Inside a quote, so other would-be terminals will just be content like anything else
+			} else if (c == '\n' || c == '\r') { // Line end
+				theLastTerminal = CsvValueTerminal.LINE_END;
+				return -1;
+			} else if (c == theDelimiter) { // Column end
+				theLastTerminal = CsvValueTerminal.COLUMN_END;
+				return -1;
 			}
 			return c;
 		}
 
-		private int countConsecutiveQuotes() throws IOException {
-			int consecutiveQuotes = 0;
-			int c = read();
-			while (c == '"') {
-				consecutiveQuotes++;
-				c = read();
+		private int readStreamChar() throws IOException {
+			int c = theReader.read();
+			if (isQuoted == null) {
+				while (c == '\r') // Ignore stupid DOS CR characters except in quotes
+					c = theReader.read();
 			}
-			if (!isLineTerminal(c))
-				theHoldover = (char) c;
-			return consecutiveQuotes;
+			return c;
 		}
-	}
 
-	private static boolean isLineTerminal(int ch) {
-		return ch < 0 || ch == '\n' || ch == '\r';
+		@Override
+		public String toString() {
+			return theValue.toString();
+		}
 	}
 }
