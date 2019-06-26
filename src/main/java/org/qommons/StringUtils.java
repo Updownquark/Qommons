ef44647a35d328b0d492fb1ea32820492f5d3805
Updownquark <Updownquark@gmail.com>
2019-06-25 22:13:35 -0600

    Added StringUtils

---
+++ src/main/java/org/qommons/StringUtils.java
@@ -0,0 +1,29 @@
+package org.qommons;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class StringUtils {
+	public static <T> StringBuilder print(CharSequence delimiter, Iterable<? extends T> values,
+		Function<? super T, ? extends CharSequence> format) {
+		return print(new StringBuilder(), delimiter, values, (v, str) -> str.append(format.apply(v)));
+	}
+
+	public static <T> StringBuilder print(StringBuilder into, CharSequence delimiter, Iterable<? extends T> values,
+		BiConsumer<? super T, ? super StringBuilder> format) {
+		if (into == null)
+			into = new StringBuilder();
+		boolean first = true;
+		for (T value : values) {
+			if (first)
+				first = false;
+			else
+				into.append(delimiter);
+			if (format != null)
+				format.accept(value, into);
+			else
+				into.append(value);
+		}
+		return into;
+	}
+}
