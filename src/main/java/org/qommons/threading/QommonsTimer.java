c918b4190c252dae4fc85c6295dfd6c998f1d08d
Updownquark <Updownquark@gmail.com>
2019-08-04 08:07:05 -0600

    Some new features for QommonsTimer

--- src/main/java/org/qommons/threading/QommonsTimer.java
+++ src/main/java/org/qommons/threading/QommonsTimer.java
@@ -9,6 +9,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.qommons.QommonsUtils;
+import org.qommons.TimeUtils;
 import org.qommons.collect.ListenerList;
 
 /** A timer class that allows very flexible scheduling of tasks without needing to create a thread per task */
@@ -24,99 +25,18 @@ public class QommonsTimer {
 	}
 
 	public interface TimerClock {
-		Time now(boolean nanoPrecision);
+		Instant now();
 	}
 
 	public static class SystemClock implements TimerClock {
 		@Override
-		public Time now(boolean nanoPrecision) {
+		public Instant now() {
 			long millis = System.currentTimeMillis();
-			long nanos;
-			if (nanoPrecision) {
-				nanos = System.nanoTime() % 1000000000;
+			long nanos = System.nanoTime() % 1000000000;
 				if (nanos < 0)
 					nanos += 1000000000;
 				nanos += millis * 1000000L;
-			} else {
-				nanos = 0;
-			}
-			return new Time(millis, nanos);
-		}
-	}
-
-	public static class Time implements Comparable<Time> {
-		public static Time ZERO = new Time(0, 0);
-		public static Time FOREVER = new Time(Long.MAX_VALUE, 0);
-
-		public final long millis;
-		public final long nanos;
-
-		public Time(long millis, long nanos) {
-			this.millis = millis;
-			this.nanos = nanos;
-		}
-
-		@Override
-		public int compareTo(Time time) {
-			int comp = Long.compare(millis, time.millis);
-			if (comp == 0) {
-				long nanoDiff = (nanos - time.nanos);
-				comp = nanoDiff < 0 ? -1 : (nanoDiff > 0 ? 1 : 0);
-			}
-			return comp;
-		}
-
-		public Time plus(Time other) {
-			return new Time(millis + other.millis, nanos + other.nanos);
-		}
-
-		public Time minus(Time other) {
-			if (millis != 0) {
-				if (other.millis != 0)
-					return new Time(millis - other.millis, 0);
-				else
-					return new Time(0, millis * 1000000 - other.nanos);
-			} else if (other.millis != 0)
-				return new Time(0, nanos - other.millis * 1000000);
-			else
-				return new Time(0, nanos - other.nanos);
-		}
-
-		public Time multipliedBy(long mult) {
-			return new Time(millis * mult, nanos * mult);
-		}
-
-		public long dividedBy(Time time) {
-			if (millis != 0) {
-				if (time.millis != 0)
-					return millis / time.millis;
-				else
-					return millis * 1000000 / time.nanos;
-			} else if (time.millis != 0)
-				return nanos / (time.millis * 1000000);
-			else
-				return nanos / time.nanos;
-		}
-
-		public static Time of(Instant time, boolean nanoPrecision) {
-			if (!nanoPrecision)
-				return new Time(time.toEpochMilli(), 0);
-			long secs = time.getEpochSecond();
-			int nanos = time.getNano();
-			return new Time(secs * 1000 + nanos / 1000, secs * 1000000000 + nanos);
-		}
-
-		public static Time of(Duration time, boolean nanoPrecision) {
-			if (!nanoPrecision)
-				return new Time(time.toMillis(), 0);
-			long secs = time.getSeconds();
-			int nanos = time.getNano();
-			return new Time(secs * 1000 + nanos / 1000, secs * 1000000000 + nanos);
-		}
-
-		@Override
-		public String toString() {
-			return millis + ":" + nanos;
+			return Instant.ofEpochSecond(millis, nanos);
 		}
 	}
 
@@ -127,28 +47,29 @@ public class QommonsTimer {
 	/** A handle on a scheduled task that allows */
 	public class TaskHandle {
 		private final Runnable theTask;
-		private volatile boolean isScheduled;
+		private final AtomicBoolean isActive;
 		private Runnable theRemove;
 
-		private Duration theFrequency;
-		private volatile Time theFrequencyTime;
+		private volatile Duration theFrequency;
 		private volatile boolean isConsistent;
 
-		private volatile Time theLastRun;
-		private volatile Time theNextRun;
+		private volatile Instant thePreviousRun;
+		private volatile Instant theNextRun;
+
+		private volatile long theRemainingExecCount;
+		private volatile Instant theLastRun;
+		private volatile boolean runAfterLast;
 
 		private volatile boolean isExecuting;
 		private volatile boolean isWaiting;
 		private volatile long theExecCount;
 		private volatile TaskThreading theThreading;
 
-		TaskHandle(Runnable task, Duration initDelay, Duration frequency, boolean consistent) {
+		TaskHandle(Runnable task) {
 			theTask = task;
-			isScheduled = true;
+			isActive = new AtomicBoolean();
 			theThreading = TaskThreading.Any;
-			runNextIn(frequency); // Gotta set this first because setFrequency uses it
-			setFrequency(frequency, consistent);
-			runNextIn(initDelay);
+			theRemainingExecCount = -1;
 		}
 
 		public QommonsTimer getTimer() {
@@ -167,25 +88,31 @@ public class QommonsTimer {
 			return isWaiting;
 		}
 
-		public boolean isScheduled() {
-			return isScheduled;
+		public boolean isActive() {
+			return isActive.get();
 		}
 
 		public Duration getTimeUntilNextRun() {
-			if (!isScheduled)
+			if (!isActive.get())
 				return null;
-			Time nextRun = theNextRun;
-			boolean nanoPrecision = nextRun.nanos != 0;
-			Time now = theClock.now(nanoPrecision);
-			long millisDiff = now.millis - nextRun.millis;
-			if (millisDiff < 0)
-				return Duration.ZERO;
-			else if (!nanoPrecision || millisDiff >= 10)
-				return Duration.ofMillis(millisDiff);
-			long nanoDiff = now.nanos - nextRun.nanos;
-			if (nanoDiff < 0)
-				return Duration.ZERO;
-			return Duration.ofNanos(nanoDiff);
+			Instant nextRun = theNextRun;
+			return nextRun == null ? null : TimeUtils.between(theClock.now(), nextRun);
+		}
+
+		public long getExecutionCount() {
+			return theExecCount;
+		}
+
+		public long getRemainingExecutionCount() {
+			return theRemainingExecCount;
+		}
+
+		public Instant getLastRun() {
+			return theLastRun;
+		}
+
+		public Instant getPreviousExecution() {
+			return thePreviousRun;
 		}
 
 		public Duration getFrequency() {
@@ -198,56 +125,62 @@ public class QommonsTimer {
 		}
 
 		public TaskHandle runImmediately() {
-			return runNextAt(theClock.now(true));
+			return runNextAt(theClock.now());
 		}
 
-		public TaskHandle runNextAt(Instant time, boolean nanoPrecision) {
-			return runNextAt(Time.of(time, nanoPrecision));
-		}
-
-		public TaskHandle runNextIn(Duration interval) {
-			if (interval.isNegative() || interval.isZero())
-				return runImmediately();
-			else {
-				boolean nanoPrecision = interval.getSeconds() == 0 && interval.getNano() < 5_000_000;
-				return runNextAt(theClock.now(nanoPrecision).plus(Time.of(interval, nanoPrecision)));
-			}
-		}
-
-		private TaskHandle runNextAt(Time time) {
-			if (!isScheduled)
-				throw new IllegalStateException("This task has been stopped");
+		public TaskHandle runNextAt(Instant time) {
 			theNextRun = time;
-			interruptScheduler();
 			return this;
 		}
 
-		public TaskHandle setFrequency(Duration frequency, boolean consistent) {
-			if (!isScheduled)
-				throw new IllegalStateException("This task has been stopped");
-			theFrequency = frequency;
-			boolean nanoPrecision = frequency.getSeconds() == 0 && frequency.getNano() < 5_000_000;
-			if (nanoPrecision) {
-				theFrequencyTime = new Time(0, frequency.toNanos());
-			} else
-				theFrequencyTime = new Time(frequency.toMillis(), 0);
-			isConsistent = consistent;
+		public TaskHandle runNextIn(Duration interval) {
+			if (interval == null || interval.isNegative() || interval.isZero())
+				return runImmediately();
+			else
+				return runNextAt(theClock.now().plus(interval));
+		}
 
-			Time nextRun = theNextRun;
-			if (nextRun != Time.FOREVER) {
-				Time now = theClock.now(nanoPrecision);
-				Time nextRun2 = now.plus(theFrequencyTime);
-				if (nextRun2.compareTo(nextRun) < 0) {
+		/**
+		 * Controls how often this periodic task executes
+		 * 
+		 * @param frequency The frequency at which to execute the task
+		 * @param consistent Whether the task should be run on a strict interval. If true, an attempt will be made to start the task at the
+		 *        beginning of the interval so that an execution happens precisely as often as described. If one execution is delayed, the
+		 *        next may happen slightly earlier to make up the time and restore the rhythm to the interval. If false, the task will run
+		 *        no more often than the given interval, i.e. the duration between the end of one execution and the beginning of another
+		 *        will be the given frequency interval or slightly longer.
+		 * @return This task handle
+		 */
+		public TaskHandle setFrequency(Duration frequency, boolean consistent) {
+			theFrequency = frequency;
+			if (frequency != null && isActive.get()) {
+				Instant nextRun2 = theClock.now().plus(frequency);
+				Instant nextRun = theNextRun;
+				if (nextRun == null || nextRun2.compareTo(nextRun) < 0)
 					theNextRun = nextRun2;
-					interruptScheduler();
-				}
 			}
+			return this;
+		}
+
+		public TaskHandle times(long times) {
+			theRemainingExecCount = times;
+			return null;
+		}
+
+		/**
+		 * Controls when this task will shut itself down
+		 * 
+		 * @param until
+		 * @param runAfterLast
+		 * @return
+		 */
+		public TaskHandle until(Instant until, boolean runAfterLast) {
+			theLastRun = until;
+			this.runAfterLast = runAfterLast;
 			return this;
 		}
 
 		public TaskHandle withThreading(TaskThreading threading) {
-			if (!isScheduled)
-				throw new IllegalStateException("This task has been stopped");
 			if (threading == null)
 				throw new NullPointerException();
 			theThreading = threading;
@@ -283,59 +216,106 @@ public class QommonsTimer {
 			return withThreading(TaskThreading.Any);
 		}
 
-		/**
-		 * Permanently removes this task from the schedule. It cannot be added again, but must be re-scheduled with a different handle. This
-		 * call does not stop the current execution if the task is currently running.
-		 */
-		public void stop() {
-			if (isScheduled) {
-				isScheduled = false;
-				theRemove.run();
-			}
+		public TaskHandle resetExecutionCount() {
+			theExecCount = 0;
+			return this;
 		}
 
-		boolean shouldExecute(Time now, Time[] minNextRun) {
-			if (!isScheduled)
+		/**
+		 * Causes this task to begin or cease executing
+		 * 
+		 * @param active Whether the task should execut or not
+		 * @return This tak handle
+		 */
+		public TaskHandle setActive(boolean active) {
+			if (isActive.compareAndSet(!active, active)) {
+				synchronized (this) {
+					if (isActive.get() != active)
+						return this;
+					if (active)
+						theRemove = schedule(this);
+					else {
+						theRemove.run();
+						theRemove = null;
+					}
+				}
+			}
+			return this;
+		}
+
+		boolean shouldExecute(Instant now, Instant[] minNextRun) {
+			if (!isActive.get())
 				return false;
 			else if (isWaiting) {
-				minNextRun[0] = Time.ZERO;
+				minNextRun[0] = Instant.MIN;
 				return false;
 			}
-			boolean execute = now.compareTo(theNextRun) >= 0;
-			if (execute) {
+			Instant nextRun = theNextRun;
+			boolean execute = now.compareTo(nextRun) >= 0;
+			Instant lastRun = theLastRun;
+			if (lastRun != null && !runAfterLast && now.compareTo(lastRun) > 0) {
+				execute = false;
+				nextRun = null;
+			} else if (execute) {
 				if (isConsistent) {
-					theNextRun = theNextRun.plus(theFrequencyTime);
-					if (now.compareTo(theNextRun) > 0) {
+					Duration freq = theFrequency;
+					nextRun = nextRun.plus(freq);
+					if (now.compareTo(nextRun) > 0) {
 						// Not keeping up; skip a run or two
-						long mult = now.minus(theNextRun).dividedBy(theFrequencyTime);
-						theNextRun = theNextRun.plus(theFrequencyTime.multipliedBy(mult));
+						long mult = TimeUtils.divide(TimeUtils.between(nextRun, now), freq);
+						nextRun = nextRun.plus(freq.multipliedBy(mult));
 						// Probably need to skip one more due to flooring
-						if (now.compareTo(theNextRun) > 0)
-							theNextRun = theNextRun.plus(theFrequencyTime);
+						if (now.compareTo(nextRun) > 0)
+							nextRun = nextRun.plus(freq);
 					}
 				} else
-					theNextRun = Time.FOREVER;
+					nextRun = null;
+				long rem = theRemainingExecCount;
+				if (rem > 0) {
+					rem--;
+					theRemainingExecCount = rem;
+					if (rem == 0)
+						nextRun = null;
+				}
 				isWaiting = true;
 			}
-			if (minNextRun[0] == null || theNextRun.compareTo(minNextRun[0]) < 0)
-				minNextRun[0] = theNextRun;
+			if (nextRun != null) {
+				if (minNextRun[0] == null || nextRun.compareTo(minNextRun[0]) < 0)
+					minNextRun[0] = nextRun;
+			} else
+				setActive(false);
 			return execute;
 		}
 
 		void execute() {
-			if (isScheduled) {
+			if (isActive.get()) {
 				isExecuting = true;
 				try {
-					theLastRun = theClock.now(theFrequencyTime.nanos != 0);
+					thePreviousRun = theClock.now();
 					theTask.run();
 				} catch (RuntimeException | Error e) {
 					e.printStackTrace();
 				}
 				theExecCount++;
-				if (theNextRun == Time.FOREVER) {
-					theNextRun = theLastRun.plus(theFrequencyTime);
-					interruptScheduler();
+				Instant nextRun;
+				if (theRemainingExecCount == 0)
+					nextRun = null;
+				else if (theNextRun == null && theFrequency != null)
+					nextRun = thePreviousRun.plus(theFrequency);
+				else
+					nextRun = theNextRun;
+				Instant lastRun = theLastRun;
+				if (nextRun != null && lastRun != null) {
+					if (thePreviousRun.compareTo(lastRun) >= 0)
+						nextRun = null;
+					else if (!runAfterLast && nextRun.compareTo(lastRun) > 0)
+						nextRun = null;
 				}
+				theNextRun = nextRun;
+				if (nextRun != null)
+					interruptScheduler();
+				else
+					setActive(false);
 				isExecuting = false;
 			}
 			isWaiting = false;
@@ -364,6 +344,8 @@ public class QommonsTimer {
 	}
 
 	/**
+	 * Begins periodic execution of a new task
+	 * 
 	 * @param task The task to execute periodically
 	 * @param frequency The frequency at which to execute the task
 	 * @param consistent Whether the task should be run on a strict interval. If true, an attempt will be made to start the task at the
@@ -378,6 +360,8 @@ public class QommonsTimer {
 	}
 
 	/**
+	 * Begins periodic execution of a new task
+	 * 
 	 * @param task The task to execute periodically
 	 * @param initialDelay The amount of time before the first execution of the task
 	 * @param frequency The frequency at which to execute the task
@@ -389,9 +373,21 @@ public class QommonsTimer {
 	 * @return The task handle to use to control or stop the task's execution
 	 */
 	public TaskHandle execute(Runnable task, Duration initialDelay, Duration frequency, boolean consistent) {
-		TaskHandle handle = new TaskHandle(task, initialDelay, frequency, consistent);
-		handle.theRemove = theTaskQueue.add(handle, false);
-		return handle;
+		return build(task).setFrequency(frequency, consistent).runNextIn(frequency).setActive(true);
+	}
+
+	/**
+	 * Creates a handle for a task, relying on the caller to begin its execution with {@link TaskHandle#setActive(boolean) setActive(true)}
+	 * 
+	 * @param task The task to execute periodically
+	 * @return The task handle to use to control or stop the task's execution
+	 */
+	public TaskHandle build(Runnable task) {
+		return new TaskHandle(task);
+	}
+
+	Runnable schedule(TaskHandle task) {
+		return theTaskQueue.add(task, false);
 	}
 
 	/** @return Whether there are any tasks scheduled in this timer */
@@ -417,13 +413,13 @@ public class QommonsTimer {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {}
 		}
-		Time[] minNextRun = new Time[1];
-		Time now = theClock.now(true);
+		Instant[] minNextRun = new Instant[1];
+		Instant now = theClock.now();
 		theSchedulerThread = Thread.currentThread();
 		isSleeping.set(false);
 		while (shouldRun) {
 			minNextRun[0] = null;
-			Time fNow = now;
+			Instant fNow = now;
 			theTaskQueue.forEach(handle -> {
 				if (handle.shouldExecute(fNow, minNextRun)) {
 					switch (handle.getThreading()) {
@@ -435,20 +431,20 @@ public class QommonsTimer {
 						break;
 					case Any:
 						if (!theAccessoryRunner.apply(handle::execute))
-							minNextRun[0] = Time.ZERO;
+							minNextRun[0] = Instant.MIN;
 						break;
 					}
 				}
 			});
-			now = theClock.now(true);
+			now = theClock.now();
 			if (minNextRun[0] != null && minNextRun[0].compareTo(now) > 0) {
-				Time sleepTime = minNextRun[0].minus(now);
+				Duration sleepTime = TimeUtils.between(now, minNextRun[0]);
 				if (isSleeping.compareAndSet(false, true)) {
 					try {
-						if (sleepTime.millis >= 5)
-							Thread.sleep(sleepTime.millis);
+						if (sleepTime.getSeconds() != 0 || sleepTime.getNano() >= 5_000_000)
+							Thread.sleep(sleepTime.toMillis());
 						else
-							Thread.sleep(0, (int) sleepTime.nanos);
+							Thread.sleep(sleepTime.getNano() / 1_000_000, sleepTime.getNano() % 1_000_000);
 					} catch (InterruptedException e) {
 					}
 				}
@@ -487,7 +483,7 @@ public class QommonsTimer {
 			while (true) {
 				String line = scanner.nextLine();
 				if ("stop".equals(line)) {
-					handle.stop();
+					handle.setActive(false);
 					break;
 				}
 				TaskThreading nextThreading = nextThreading(handle.getThreading());
