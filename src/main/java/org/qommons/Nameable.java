fea92f3b1d8fe049dd1f57a0ca891791f9cb376f
abutler <abutler@psl.nmsu.edu>
2020-01-31 16:31:28 -0700

    A little improvement in Named, plus javadoc

--- src/main/java/org/qommons/Nameable.java
+++ src/main/java/org/qommons/Nameable.java
@@ -1,5 +1,10 @@
 package org.qommons;
 
+/** A named object whose name is settable directly */
 public interface Nameable extends Named {
+	/**
+	 * @param name The name for this object
+	 * @return This object
+	 */
 	Nameable setName(String name);
 }
--- src/main/java/org/qommons/Named.java
+++ src/main/java/org/qommons/Named.java
@@ -1,5 +1,7 @@
 package org.qommons;
 
+import java.util.Comparator;
+
 /** An item that has a name */
 public interface Named {
 	/** @return The name of the item */
@@ -28,4 +30,8 @@ public interface Named {
 			return getName();
 		}
 	}
+
+	/** A comparator to sort named items in a way that sorts embedded numbers well */
+	public static final Comparator<Named> DISTINCT_NUMBER_TOLERANT = (o1, o2) -> StringUtils.compareNumberTolerant(o1.getName(),
+		o2.getName(), true, true);
 }
