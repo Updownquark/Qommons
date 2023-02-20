6e1c5033d468dd86bcfb11fdf4cec3c4f8e0b982
Andrew Butler <abutler@sgc.nmsu.edu>
2023-02-20 09:17:37 -0700

    Improvement to getting local network address

--- src/main/java/org/qommons/io/LocalNetworkAddress.java
+++ src/main/java/org/qommons/io/LocalNetworkAddress.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Enumeration;
+import java.util.List;
 
 import org.qommons.Named;
 import org.qommons.collect.BetterCollections;
@@ -35,6 +38,18 @@ public interface LocalNetworkAddress extends Named {
 	StringBuilder printAddress(StringBuilder str);
 
 	/**
+	 * @return A string containing this address's host name and IP address in the form 'hostname@address', or simply 'address' if the host
+	 *         name was not resolved
+	 */
+	default String printNameAndAddress() {
+		String addr = printAddress(null).toString();
+		String host = getName();
+		if (host == null || host.equals(addr))
+			return addr;
+		return new StringBuilder(host).append('@').append(addr).toString();
+	}
+
+	/**
 	 * <p>
 	 * A more reliable way to get local addresses than {@link InetAddress#getLocalHost()}. The Java method can be fooled under some
 	 * configurations, e.g. cloned VMs.
@@ -52,17 +67,16 @@ public interface LocalNetworkAddress extends Named {
 	 * @param maxIpVersion The maximum IP version to include
 	 * @param cached Whether to use the cached version, if this method has previously been called
 	 * @return The list of reachable, non-loopback network addresses for this host
-	 * @throws SocketException If the list of network interfaces could not be retrieved
+	 * @throws SocketException If an error occurs retrieving the list of network interfaces
 	 */
 	public static BetterList<LocalNetworkAddress> getLocalAddresses(int minIpVersion, int maxIpVersion, boolean cached)
 		throws SocketException {
 		BetterList<LocalNetworkAddress> addresses;
-		if (cached && Internal.CACHED != null) {
-			addresses = Internal.CACHED;
+		if (cached && Internal.CACHED_ALL != null) {
+			addresses = Internal.CACHED_ALL;
 		} else {
 			addresses = BetterTreeList.<LocalNetworkAddress> build().build();
 			Enumeration<NetworkInterface> intfs = NetworkInterface.getNetworkInterfaces();
-			boolean excludedByVersion = false;
 			while (intfs.hasMoreElements()) {
 				NetworkInterface intf = intfs.nextElement();
 				if (intf.isLoopback())
@@ -87,14 +101,10 @@ public interface LocalNetworkAddress extends Named {
 						continue;
 					}
 					try {
-						if (ipVersion < minIpVersion || ipVersion > maxIpVersion) {
-							excludedByVersion = true;
-							continue;
-						}
 						// This timeout seems long, but this call is actually small fries compared to the rest of this method.
 						// Using a larger value here should make this method more reliable,
 						// while still not contributing significantly to the length of this call
-						else if (!addr.isReachable(50))
+						if (!addr.isReachable(50))
 							continue;
 					} catch (IOException e) {
 						continue;// Do nothing, just don't include the address
@@ -111,12 +121,102 @@ public interface LocalNetworkAddress extends Named {
 				}
 			}
 			addresses = BetterCollections.unmodifiableList(addresses);
-			if (!excludedByVersion)
-				Internal.CACHED = addresses;
+			Internal.CACHED_ALL = addresses;
 		}
 		if (minIpVersion <= 0 && maxIpVersion >= 6)
 			return addresses;
 		return BetterList.of(addresses.stream().filter(addr -> addr.getIpVersion() >= minIpVersion && addr.getIpVersion() <= maxIpVersion));
+	}
+
+	/**
+	 * @param minIpVersion The minimum IP version to include
+	 * @param maxIpVersion The maximum IP version to include
+	 * @param cached Whether to use the cached version, if this method has previously been called
+	 * @return The first found reachable, non-loopback network address for this host within an IP version in the given range
+	 * @throws SocketException If an error occurs retrieving the network interface
+	 * @see #getLocalAddresses(int, int, boolean)
+	 */
+	public static LocalNetworkAddress getLocalAddress(int minIpVersion, int maxIpVersion, boolean cached) throws SocketException {
+		if (cached) {
+			if (Internal.CACHED_FIRST.size() > minIpVersion)
+				return Internal.CACHED_FIRST.get(Math.min(maxIpVersion, Internal.CACHED_FIRST.size() - 1));
+			else if (Internal.CACHED_ALL != null) {
+				// Return the first, highest-version address that matches the query
+				int bestVersion = 0;
+				LocalNetworkAddress bestAddr = null;
+				for (LocalNetworkAddress addr : Internal.CACHED_ALL) {
+					if (addr.getIpVersion() < minIpVersion || addr.getIpVersion() > maxIpVersion)
+						continue;
+					if (addr.getIpVersion() > bestVersion) {
+						bestVersion = addr.getIpVersion();
+						bestAddr = addr;
+					}
+				}
+				if (bestAddr != null)
+					return bestAddr;
+			}
+		}
+		synchronized (Internal.class) {
+			Enumeration<NetworkInterface> intfs = NetworkInterface.getNetworkInterfaces();
+			BitSet replacedIpVersions = new BitSet();
+			while (intfs.hasMoreElements()) {
+				NetworkInterface intf = intfs.nextElement();
+				if (intf.isLoopback())
+					continue;
+				else if (!intf.isUp())
+					continue;
+				Enumeration<InetAddress> addrs = intf.getInetAddresses();
+				String intfName = null, intfDisplayName = null;
+				boolean triedIntfName = false, triedIntfDisplayName = false;
+				while (addrs.hasMoreElements()) {
+					InetAddress addr = addrs.nextElement();
+					int ipVersion;
+					switch (addr.getAddress().length) {
+					case 4:
+						ipVersion = 4;
+						break;
+					case 16:
+						ipVersion = 6;
+						break;
+					default:
+						System.err.println("Unrecognized address version: " + Arrays.toString(addr.getAddress()));
+						continue;
+					}
+					try {
+						// This timeout seems long, but this call is actually small fries compared to the rest of this method.
+						// Using a larger value here should make this method more reliable,
+						// while still not contributing significantly to the length of this call
+						if (!addr.isReachable(50))
+							continue;
+					} catch (IOException e) {
+						continue;// Do nothing, just don't include the address
+					}
+					if (!triedIntfName) {
+						triedIntfName = true;
+						intfName = intf.getName();
+					}
+					if (!triedIntfDisplayName) {
+						triedIntfDisplayName = true;
+						intfDisplayName = intf.getDisplayName();
+					}
+					LocalNetworkAddress lna = null;
+					// The CACHED_FIRST list is the first address of the each version
+					if (!replacedIpVersions.get(ipVersion)) {
+						lna = new Default(intfName, intfDisplayName, ipVersion, addr.getHostName(), addr.getAddress());
+						replacedIpVersions.set(ipVersion);
+						while (Internal.CACHED_FIRST.size() <= ipVersion)
+							Internal.CACHED_FIRST.add(null);
+						Internal.CACHED_FIRST.set(ipVersion, lna);
+					}
+					if (ipVersion >= minIpVersion && ipVersion <= maxIpVersion) {
+						if (lna == null)
+							lna = new Default(intfName, intfDisplayName, ipVersion, addr.getHostName(), addr.getAddress());
+						return lna;
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	/** Default implementation */
@@ -265,7 +365,8 @@ public interface LocalNetworkAddress extends Named {
 
 	/** Internal resources for use by {@link LocalNetworkAddress} */
 	class Internal {
-		static BetterList<LocalNetworkAddress> CACHED;
+		static BetterList<LocalNetworkAddress> CACHED_ALL;
+		static final List<LocalNetworkAddress> CACHED_FIRST = new ArrayList<>();
 
 		private Internal() {
 		}
