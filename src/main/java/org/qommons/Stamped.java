d973b640af5f5a1a07b4063c7dc941999fdc22fc
Updow <Updow@AndrewLaptop202>
2023-06-15 09:46:05 -0400

    Improved the composite stamp function to use both XOR and addition, which fixed a bug in Quick styles that were causing value stamps in composite structure to annihilate each other, resulting in composite structures that were unaware they had changed

--- src/main/java/org/qommons/Stamped.java
+++ src/main/java/org/qommons/Stamped.java
@@ -1,7 +1,10 @@
 package org.qommons;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.PrimitiveIterator;
 import java.util.function.ToLongFunction;
+import java.util.stream.LongStream;
 
 /** An Object that provides a stamp that changes when it is modified */
 public interface Stamped {
@@ -27,6 +30,17 @@ public interface Stamped {
 	 * Creates a composite stamp from a collection of other stamped items
 	 * 
 	 * @param <T> The type of the stamped values
+	 * @param values The stamped values to create the composite stamp for
+	 * @return The composite stamp
+	 */
+	public static <T> long compositeStamp(Collection<? extends Stamped> values) {
+		return compositeStamp(values, Stamped::getStamp);
+	}
+
+	/**
+	 * Creates a composite stamp from a collection of other stamped items
+	 * 
+	 * @param <T> The type of the stamped values
 	 * @param values The values to create the composite stamp for
 	 * @param stampFn The function to produce stamps from the values
 	 * @return The composite stamp
@@ -34,44 +48,7 @@ public interface Stamped {
 	public static <T> long compositeStamp(Collection<? extends T> values, ToLongFunction<? super T> stampFn) {
 		if (values.isEmpty())
 			return 0;
-		long stamp = 0;
-		int shift = Math.max(1, 63 / values.size());
-		int i = 0;
-		for (T value : values) {
-			if (value == null)
-				continue;
-			long valueStamp = stampFn.applyAsLong(value);
-			if (i > 0)
-				valueStamp = Long.rotateRight(valueStamp, shift * i);
-			stamp ^= valueStamp;
-			i++;
-		}
-		return stamp;
-	}
-
-	/**
-	 * Creates a composite stamp from a collection of other stamped items
-	 * 
-	 * @param <T> The type of the stamped values
-	 * @param values The stamped values to create the composite stamp for
-	 * @return The composite stamp
-	 */
-	public static <T> long compositeStamp(Collection<? extends Stamped> values) {
-		if (values.isEmpty())
-			return 0;
-		long stamp = 0;
-		int shift = Math.max(1, 63 / values.size());
-		int i = 0;
-		for (Stamped value : values) {
-			if (value == null)
-				continue;
-			long valueStamp = value.getStamp();
-			if (i > 0)
-				valueStamp = Long.rotateRight(valueStamp, shift * i);
-			stamp ^= valueStamp;
-			i++;
-		}
-		return stamp;
+		return compositeStamp(values.stream().mapToLong(stampFn), values.size());
 	}
 
 	/**
@@ -83,13 +60,31 @@ public interface Stamped {
 	static long compositeStamp(long... stamps) {
 		if (stamps.length == 0)
 			return 0;
+		return compositeStamp(Arrays.stream(stamps), stamps.length);
+	}
+
+	/**
+	 * Creates a composite stamp from a stream of stamps
+	 * 
+	 * @param stamps The stamps to create the composite stamp for
+	 * @param count The number of stamps in the stream
+	 * @return The composite stamp
+	 */
+	static long compositeStamp(LongStream stamps, int count) {
+		PrimitiveIterator.OfLong stampsIter = stamps.iterator();
 		long stamp = 0;
-		int shift = Math.max(1, 63 / stamps.length);
+		int shift = Math.max(1, 63 / count);
 		int i = 0;
-		for (long s : stamps) {
-			if (i > 0)
-				s = Long.rotateRight(s, shift * i);
-			stamp ^= s;
+		// I'm changing this to use both XOR and addition operators in generating the composite stamp
+		// When using XOR only, I found that in some cases, e.g. with composite structures as op1(op2(v1, v2), op2(v1)),
+		// a value's stamp can annihilate itself in the composite. Addition prevents this.
+		// The XOR operation is still necessary so that ordering of the stamps is important.
+		while (stampsIter.hasNext()) {
+			long s = stampsIter.nextLong();
+			if (i == 0)
+				stamp = s;
+			else
+				stamp = (stamp ^ Long.rotateRight(s, shift * i)) + s;
 			i++;
 		}
 		return stamp;
